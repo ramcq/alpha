5d3c21c6f38b4b1b0ee186e0227faae587491e23
sjc209 <sjc209>
2004-02-03 16:22:34 +0000

    *** empty log message ***

--- src/org/ucam/ned/teamalpha/animators/VectorAnimator.java
+++ src/org/ucam/ned/teamalpha/animators/VectorAnimator.java
@@ -4,18 +4,25 @@ public abstract class VectorAnimator extends Animator {
 	public abstract class Vector {
 		public abstract void delete();
 		public abstract void setLabel(String label);
-		public abstract Pointer createPointer(int offset);
-		public abstract Pointer createPointer(int offset, boolean visible);
+		public abstract Arrow createPoi(int offset, boolean boundary);
+		public abstract Arrow createPointer(int offset, boolean visible, boolean boundary);
+		public abstract void setHighlightColumns(int colour, int column);
+		public abstract Vector splitVector(int offset);
+		public abstract void swapElements(int offset1, int offset2);
+		public abstract void swapElements(int offset1, int offset2, Vector Vector2);
+		public abstract void moveElement(int offsetfrom, int offsetto);
+		
 	}
 	
 	public abstract class Pointer {
 		public abstract void setVisible(boolean visible);
-		public abstract void setValue(int value);
-		public abstract void copyTo(Pointer p);
-		public abstract void swapWith(Pointer p);
+		public abstract void highlightPointer(boolean highlight);
+		public abstract void movePointer(int offset);
+		public abstract void setLabel(String label);
+		public abstract void deletePointer();
 		
 	}
-
+	
 	public abstract Vector createVector(int[] values);
 	public abstract Vector createVector(String label, int[] values);
 	
