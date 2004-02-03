a742835838c4c0bed09e61a6fb6bfa31e4dd00d1
ram48 <ram48>
2004-02-03 16:41:40 +0000

    finished vector animator API?

--- src/org/ucam/ned/teamalpha/animators/VectorAnimator.java
+++ src/org/ucam/ned/teamalpha/animators/VectorAnimator.java
@@ -4,25 +4,27 @@ public abstract class VectorAnimator extends Animator {
 	public abstract class Vector {
 		public abstract void delete();
 		public abstract void setLabel(String label);
+		
+		public abstract void copyElement(int offsetfrom, int offsetto);
+		public abstract void copyElement(int offsetfrom, Vector target, int offsetto);
+		public abstract void moveElement(int offsetfrom, int offsetto);
+		public abstract void setElement(int offset, int value);
+		public abstract void swapElements(int offset1, int offset2);
+		public abstract void swapElements(int offset1, Vector target, int offset2);
+		
+		public abstract void setHighlightedColumns(int[] columns);
+		public abstract Vector splitVector(int offset);
+		
 		public abstract Arrow createArrow(int offset, boolean boundary);
 		public abstract Arrow createArrow(int offset, boolean boundary, String label);
-		public abstract void setHighlightColumns(int colour, int column);
-		public abstract Vector splitVector(int offset);
-		public abstract void swapElements(int offset1, int offset2);
-		public abstract void swapElements(int offset1, int offset2, Vector Vector2);
-		public abstract void moveElement(int offsetfrom, int offsetto);	
 	}
 	
 	public abstract class Arrow {
 		public abstract void delete();
 		public abstract void setLabel(String label);
-		public abstract String getLabel();
 		public abstract void setOffset(int offset);
-		public abstract int getOffset();
 		public abstract void setBoundary(boolean boundary);
-		public abstract boolean getBoundary();
 		public abstract void setHighlight(boolean highlight);
-		public abstract boolean getHighlight();
 	}
 	
 	public abstract Vector createVector(int[] values);
