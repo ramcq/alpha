957d8b41ff059519debdc24891c1248bd035a29e
ram48 <ram48>
2004-02-04 11:39:46 +0000

    Package names.

--- src/org/ucam/ned/teamalpha/algorithms/QuickSort.java
+++ src/org/ucam/ned/teamalpha/algorithms/QuickSort.java
@@ -4,9 +4,9 @@
  * To change the template for this generated file go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-package src.org.ucam.ned.teamalpha.algorithms;
+package org.ucam.ned.teamalpha.algorithms;
 
-import ucam.org.ned.teamalpha.algorithms.*;
+import org.ucam.ned.teamalpha.algorithms.*;
 
 /**
  * @author sas58
