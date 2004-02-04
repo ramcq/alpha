e8715f8dfc0ad7c0c56e4cb096785d477ca79f83
sas58 <sas58>
2004-02-04 22:46:08 +0000

    * Fleshed out the quicksort code, and tested it pretty thoroughly. * Added some javadoc comments, please let me know if my syle is ambiguous/boring/insufficient/plain crap * Added TODO statements at the places where I will need to call on the relevant animator methods

--- src/org/ucam/ned/teamalpha/algorithms/QuickSort.java
+++ src/org/ucam/ned/teamalpha/algorithms/QuickSort.java
@@ -1,8 +1,6 @@
 /*
  * Created on Feb 4, 2004
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 package org.ucam.ned.teamalpha.algorithms;
 
@@ -11,18 +9,87 @@ import org.ucam.ned.teamalpha.animators.VectorAnimator;
 /**
  * @author sas58
  *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 public class QuickSort extends VectorAlgorithm {
 
+	private int[] a;
+	
+	/**
+	 * The core of the quicksort algorithm. <class>quick</class> partitions
+	 * the array between two offsets and then recursively calls
+	 * itself on the two halves.
+	 * 
+	 * @param low
+	 * 	The offset of the lower bound of the array sector
+	 * 	that is to be sorted in this current pass.
+	 * @param high
+	 * 
+	 * 	The offset of the upper bound of the array sector
+	 * 	that is to be sorted in this current pass.
+	 */
+	private void quick(int low, int high)	{
+		int i = low;
+		int j = high;
+		int pivot;
+
+		if ( high > low) {
+
+			// Select the pivot by the median of first, middle and last element
+			pivot = a[ ( low + high ) / 2 ];
+
+			// Partition the vector
+			while( i <= j )
+			{
+
+				// Search for elements to swap
+				while( ( i < high ) && ( a[i] < pivot ) )	++i; // TODO: Move the arrow right
+				while( ( j > low ) && ( a[j] > pivot ) ) --j; // TODO: Move the arrow left
+
+				// if the indexes have not crossed, swap
+				if( i <= j ) 
+				{
+					swap(i, j); // TODO: Swap elements
+					++i; // TODO: Move the arrow right
+					--j; // TODO: Move the arrow left
+				}
+			}
+
+			// Recursively quicksort if pointers are not at edges
+			// Need additional logic to prevent partitioning the vector too many times
+			if( low < j ) {
+				quick(low, j );  // TODO: Partition vector at j+1
+				if( i < high )	quick( i, high );
+			}
+			else {
+				if( i < high )	quick( i, high ); // TODO: Partition vector at i-1
+			}
+			
+		}
+	}
+	
+	/**
+	 * Performs the swap of two elements in the local array.
+	 * 
+	 * @param i
+	 * 	The offset of  first element of the pair to be swapped.
+	 * @param j
+	 * 	The offset of the second element of the pair to be swapped.
+	 */
+	private void swap(int i, int j) {
+		int tmp;
+		tmp = a[i]; 
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	
 	/**
 	 * @param va
 	 * @param values
 	 */
 	public QuickSort(VectorAnimator va, int[] values) {
 		super(va, values);
-		// TODO Auto-generated constructor stub
+		this.a = values;
 	}
 
 	/* (non-Javadoc)
@@ -30,23 +97,21 @@ public class QuickSort extends VectorAlgorithm {
 	 */
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return "QuickSort";
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.algorithms.Algorithm#getDescription()
 	 */
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return "TODO: [QuickSort.getDescription()] : Documenters, decide on what string to have here";
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.algorithms.Algorithm#execute()
 	 */
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		quick(0, a.length - 1);
 	}
 
 }
