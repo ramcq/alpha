0ea38a262258004bb6a4b4552c676b9d3e18c907
sas58 <sas58>
2004-02-04 00:26:14 +0000

    Added the main sort algorithms. Eclipse keeps bugging me about things not being implemented and the package path being wrong so I am testing the code in a separate (non CVS) project until things are resolved by Rob.

---
+++ src/org/ucam/ned/teamalpha/algorithms/QuickSort.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Feb 4, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package src.org.ucam.ned.teamalpha.algorithms;
+
+import ucam.org.ned.teamalpha.algorithms.*;
+
+/**
+ * @author sas58
+ *
+ */
+public class QuickSort extends VectorAlgorithm {
+
+}
