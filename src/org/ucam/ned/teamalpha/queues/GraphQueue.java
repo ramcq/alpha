5fd2792218d082f283b6bc69c283d610cec681d1
ram48 <ram48>
2004-02-07 18:21:36 +0000

    Method stubs and generic queue constructors.

--- src/org/ucam/ned/teamalpha/queues/GraphQueue.java
+++ src/org/ucam/ned/teamalpha/queues/GraphQueue.java
@@ -3,4 +3,105 @@ package org.ucam.ned.teamalpha.queues;
 import org.ucam.ned.teamalpha.animators.GraphAnimator;
 
 public class GraphQueue extends GraphAnimator implements AnimatorQueue {
+	GenericQueue q;
+	
+	public GraphQueue(GraphAnimator ga) {
+		q = new GenericQueue(this, ga);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#createGraph(int[][])
+	 */
+	public void createGraph(int[][] costs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeLabel(int, java.lang.String)
+	 */
+	public void setNodeLabel(int node, String label) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeHighlight(int, boolean)
+	 */
+	public void setNodeHighlight(int node, boolean highlight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeShade(int, int)
+	 */
+	public void setNodeShade(int node, int set) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeLabel(int, int, java.lang.String)
+	 */
+	public void setEdgeLabel(int from, int to, String label) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeHighlight(int, int, boolean)
+	 */
+	public void setEdgeHighlight(int from, int to, boolean highlight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeShade(int, int, int)
+	 */
+	public void setEdgeShade(int from, int to, int set) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#setSteps(java.lang.String[])
+	 */
+	public void setSteps(String[] steps) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#setCurrentStep(int)
+	 */
+	public void setCurrentStep(int step) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#showMessage(java.lang.String)
+	 */
+	public void showMessage(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#saveState()
+	 */
+	public State saveState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#restoreState(org.ucam.ned.teamalpha.animators.Animator.State)
+	 */
+	public void restoreState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/org/ucam/ned/teamalpha/queues/VectorQueue.java
+++ src/org/ucam/ned/teamalpha/queues/VectorQueue.java
@@ -3,4 +3,65 @@ package org.ucam.ned.teamalpha.queues;
 import org.ucam.ned.teamalpha.animators.VectorAnimator;
 
 public class VectorQueue extends VectorAnimator implements AnimatorQueue {
+	GenericQueue q;
+	
+	public VectorQueue(VectorAnimator va) {
+		q = new GenericQueue(this, va);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.VectorAnimator#createVector(int[])
+	 */
+	public Vector createVector(int[] values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.VectorAnimator#createVector(java.lang.String, int[])
+	 */
+	public Vector createVector(String label, int[] values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#setSteps(java.lang.String[])
+	 */
+	public void setSteps(String[] steps) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#setCurrentStep(int)
+	 */
+	public void setCurrentStep(int step) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#showMessage(java.lang.String)
+	 */
+	public void showMessage(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#saveState()
+	 */
+	public State saveState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.animators.Animator#restoreState(org.ucam.ned.teamalpha.animators.Animator.State)
+	 */
+	public void restoreState(State state) {
+		// TODO Auto-generated method stub
+		
+	}
 }
