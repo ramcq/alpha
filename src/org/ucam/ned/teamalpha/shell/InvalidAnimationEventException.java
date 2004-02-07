ef51129042b1eb4caa8677c8a87747ef1da4808f
am502 <am502>
2004-02-07 19:23:20 +0000

    Generic exception to be thrown when an attempt to generate an invalid animation event is made. If this is thrown, it will be my fault and no-one else's.

---
+++ src/org/ucam/ned/teamalpha/shell/InvalidAnimationEventException.java
@@ -0,0 +1,50 @@
+/*
+ * Created on Feb 7, 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package org.ucam.ned.teamalpha.shell;
+
+/**
+ * @author igor
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class InvalidAnimationEventException extends Exception {
+
+	/**
+	 * 
+	 */
+	public InvalidAnimationEventException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public InvalidAnimationEventException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public InvalidAnimationEventException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public InvalidAnimationEventException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
