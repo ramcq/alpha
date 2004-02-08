98b9c86ab1661070db719a64ef5ac53a37030c4e
ram48 <ram48>
2004-02-08 01:20:05 +0000

    Hi, I'm the Adapter design pattern. You may remember me from such lecture courses as "Concurrent Systems and Applications".
    If this actually works.... heh.... wow. :)

--- src/org/ucam/ned/teamalpha/queues/GraphQueue.java
+++ src/org/ucam/ned/teamalpha/queues/GraphQueue.java
@@ -13,80 +13,80 @@ public class GraphQueue extends GraphAnimator implements AnimatorQueue {
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#createGraph(int[][])
 	 */
 	public void createGraph(int[][] costs) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { costs };
+		q.enqueue(this, "createGraph", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeLabel(int, java.lang.String)
 	 */
 	public void setNodeLabel(int node, String label) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(node), label };
+		q.enqueue(this, "setNodeLabel", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeHighlight(int, boolean)
 	 */
 	public void setNodeHighlight(int node, boolean highlight) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(node), new Boolean(highlight) };
+		q.enqueue(this, "setNodeHighlight", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setNodeShade(int, int)
 	 */
 	public void setNodeShade(int node, int set) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(node), new Integer(set) };
+		q.enqueue(this, "setNodeShade", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeLabel(int, int, java.lang.String)
 	 */
 	public void setEdgeLabel(int from, int to, String label) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(from), new Integer(to), label };
+		q.enqueue(this, "setEdgeLabel", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeHighlight(int, int, boolean)
 	 */
 	public void setEdgeHighlight(int from, int to, boolean highlight) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(from), new Integer(to), new Boolean(highlight) };
+		q.enqueue(this, "setEdgeHighlight", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.GraphAnimator#setEdgeShade(int, int, int)
 	 */
 	public void setEdgeShade(int from, int to, int set) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(from), new Integer(to), new Integer(set) };
+		q.enqueue(this, "setEdgeShade", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.Animator#setSteps(java.lang.String[])
 	 */
 	public void setSteps(String[] steps) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { steps };
+		q.enqueue(this, "setSteps", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.Animator#setCurrentStep(int)
 	 */
 	public void setCurrentStep(int step) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { new Integer(step) };
+		q.enqueue(this, "setCurrentStep", args, null);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ucam.ned.teamalpha.animators.Animator#showMessage(java.lang.String)
 	 */
 	public void showMessage(String msg) {
-		// TODO Auto-generated method stub
-		
+		Object[] args = { msg };
+		q.enqueue(this, "showMessage", args, null);	
 	}
 
 	/* (non-Javadoc)
@@ -101,7 +101,6 @@ public class GraphQueue extends GraphAnimator implements AnimatorQueue {
 	 * @see org.ucam.ned.teamalpha.animators.Animator#restoreState(org.ucam.ned.teamalpha.animators.Animator.State)
 	 */
 	public void restoreState(State state) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 }
