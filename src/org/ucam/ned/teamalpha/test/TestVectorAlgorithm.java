71e947ff4654392f5ba99de9da846fa7f458a188
ram48 <ram48>
2004-02-12 02:14:29 +0000

    Move into appropriate package, ironically spawning more errors than it fixes.

--- src/org/ucam/ned/teamalpha/test/TestVectorAlgorithm.java
+++ src/org/ucam/ned/teamalpha/test/TestVectorAlgorithm.java
@@ -4,10 +4,10 @@
  * To change the template for this generated file go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-package src.org.ucam.ned.teamalpha.test;
+package org.ucam.ned.teamalpha.test;
 
-import src.org.ucam.ned.teamalpha.algorithms.VectorAlgorithm;
-import src.org.ucam.ned.teamalpha.animators.VectorAnimator;
+import org.ucam.ned.teamalpha.algorithms.VectorAlgorithm;
+import org.ucam.ned.teamalpha.animators.VectorAnimator;
 
 /**
  * @author ajit2
