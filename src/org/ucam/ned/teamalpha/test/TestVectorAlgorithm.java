5371c37b1ad6e2313963082c99c4e97f0aaa1e5d
ajit2 <ajit2>
2004-02-16 19:26:36 +0000

    *** empty log message ***

--- src/org/ucam/ned/teamalpha/test/TestVectorAlgorithm.java
+++ src/org/ucam/ned/teamalpha/test/TestVectorAlgorithm.java
@@ -19,7 +19,6 @@ public class TestVectorAlgorithm extends VectorAlgorithm {
 	VectorAnimator va;
 	int[] values;
 
-
 	/**
 	* @param va
 	* @param values
@@ -35,7 +34,24 @@ public class TestVectorAlgorithm extends VectorAlgorithm {
 	 * @see org.ucam.ned.teamalpha.algorithms.Algorithm#execute()
 	 */
 	public void execute() {
-	// TODO fill this out with the calls to the vector animator
 	
+		VectorAnimator.Vector v1 = va.createVector("hello", values);
+		VectorAnimator.Arrow a1 = v1.createArrow(1, false);
+		VectorAnimator.Arrow a2 = v1.createArrow(5, false);
+		
+		v1.moveElement(2, 3);
+		//v1.setElement(7, 99);			
+				
+		a2.flash();
+		a1.flash();
+		v1.flashElement(2);
+		VectorAnimator.Vector v2 = va.createVector("vector 2", values);
+		v2.delete();
+		v2.setElement(6, 100);
+		
+		//VectorAnimator.Vector v3 = va.createVector("vector 2", values);
+		
+				
 	}
+		
 }
