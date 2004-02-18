fdd7678efad3d482d9f36caed41d78574de5cd8d
ram48 <ram48>
2004-02-18 13:35:20 +0000

    Made this into an abstract class.

--- src/org/ucam/ned/teamalpha/shell/AlgorithmCatalog.java
+++ src/org/ucam/ned/teamalpha/shell/AlgorithmCatalog.java
@@ -8,6 +8,7 @@ package org.ucam.ned.teamalpha.shell;
 
 import javax.swing.JPanel;
 
+import org.ucam.ned.teamalpha.algorithms.Algorithm;
 import org.ucam.ned.teamalpha.animators.Animator;
 import org.ucam.ned.teamalpha.queues.AnimatorQueue;
 
@@ -17,16 +18,20 @@ import org.ucam.ned.teamalpha.queues.AnimatorQueue;
  * To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-interface AlgorithmCatalog {
-	Class[] getAlgorithms();
+public abstract class AlgorithmCatalog {
+	public abstract class AvailableAlgorithm {
+		public abstract String getName();
+		
+		public abstract String getDescription();
+		
+		public abstract Algorithm getAlgorithm();
+		
+		public abstract JPanel getDataInput();
+		
+		public abstract AnimatorQueue getQueue();
+		
+		public abstract Animator getAnimator();
+	}
 	
-	String getName(Class algorithm);
-	
-	String getDescription(Class algorithm);
-	
-	JPanel getDataInput(Class algorithm);
-	
-	Animator getAnimator(Class algorithm);
-	
-	AnimatorQueue getQueue(Class algorithm);
+	public abstract AvailableAlgorithm[] getAvailableAlgorithms();
 }
