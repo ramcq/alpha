81087a7fd159f390b29c99809d3dbd4094dce1c7
zrll2 <zrll2>
2004-02-18 17:58:43 +0000

    Added KruskalTest class to test for Sid's Kruskal algorithm

---
+++ src/org/ucam/ned/teamalpha/test/KruskalTest.java
@@ -0,0 +1,39 @@
+package org.ucam.ned.teamalpha.test;
+
+import org.ucam.ned.teamalpha.algorithms.GraphAlgorithm;
+import org.ucam.ned.teamalpha.animators.Animator;
+import org.ucam.ned.teamalpha.algorithms.Kruskal;
+
+/**
+ * @author richie
+ * Created on 18-Feb-2004
+ */
+public class KruskalTest extends GraphAlgorithm {
+
+	private static Animator anim;
+	
+	public KruskalTest(int[][] costs) {
+		super(costs);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.ucam.ned.teamalpha.algorithms.Algorithm#execute(org.ucam.ned.teamalpha.animators.Animator)
+	 */
+	public void execute(Animator anim) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		int[][] c ={{0,  0,  0,  0,  0,  0},
+						{5,  0,  0,  0,  0,  0},
+						{0, 10,  0,  0,  0, 0},
+						{0,  0,  6,  0,  0,  0},
+						{0,  1, 10, 12,  0,0},
+						{10, 2, 15,  0,  8,0}};
+		
+		Kruskal k = new Kruskal(c);
+		k.execute(anim);
+	}
+}
