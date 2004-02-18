da3cf5fd25bc121aaa04354c58054eb51ee00aa8
ram48 <ram48>
2004-02-18 15:23:22 +0000

    ROFLMAO
    I just saw the first attempt at animating insertion
    sort. Inserting what where I don't know. It wasn't
    sorted either.
    
    Anyway, this is the abstract class for the shell to
    find available algorithms. I'm not coherent enough
    to document it, so I'll try implementing it first.

--- src/org/ucam/ned/teamalpha/shell/AlgorithmCatalog.java
+++ src/org/ucam/ned/teamalpha/shell/AlgorithmCatalog.java
@@ -13,25 +13,48 @@ import org.ucam.ned.teamalpha.animators.Animator;
 import org.ucam.ned.teamalpha.queues.AnimatorQueue;
 
 /**
+ * 
  * @author ram48
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
  */
 public abstract class AlgorithmCatalog {
+	/**
+	 * @author ram48
+	 *
+	 * To change the template for this generated type comment go to
+	 * Window - Preferences - Java - Code Generation - Code and Comments
+	 */
 	public abstract class AvailableAlgorithm {
+		/**
+		 * @return
+		 */
 		public abstract String getName();
 		
+		/**
+		 * @return
+		 */
 		public abstract String getDescription();
+	
+		/**
+		 * @param panel
+		 * @return
+		 */
+		public abstract Algorithm getAlgorithm(JPanel panel);
+				
+		/**
+		 * @param anim
+		 * @return
+		 */
+		public abstract AnimatorQueue getQueue(Animator anim);
 		
-		public abstract Algorithm getAlgorithm();
-		
-		public abstract JPanel getDataInput();
-		
-		public abstract AnimatorQueue getQueue();
-		
-		public abstract Animator getAnimator();
+		/**
+		 * @param panel
+		 * @return
+		 */
+		public abstract Animator getAnimator(JPanel panel);
 	}
 	
+	/**
+	 * @return
+	 */
 	public abstract AvailableAlgorithm[] getAvailableAlgorithms();
 }
