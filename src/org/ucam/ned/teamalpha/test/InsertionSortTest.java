f0f7cd60c940cb0ad0171949d5d4fde4d8b8f19b
sas58 <sas58>
2004-02-19 02:41:12 +0000

    * Minor changes to the InsertionSortTest class

--- src/org/ucam/ned/teamalpha/test/InsertionSortTest.java
+++ src/org/ucam/ned/teamalpha/test/InsertionSortTest.java
@@ -7,7 +7,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import org.ucam.ned.teamalpha.algorithms.InsertionSort;
 import org.ucam.ned.teamalpha.shell.ShellVectorAnimator;
